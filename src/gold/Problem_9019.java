7fc0e8670d5b776b9b3e032e5eb3877ff2efe48d
winteeeee <b12e34e56@naver.com>
2022-08-13 17:15:01 +0900

    G4 9019 해결

--- src/gold/Problem_9019.java
+++ src/gold/Problem_9019.java
@@ -17,7 +17,7 @@ public class Problem_9019 {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            boolean[] visited = new boolean[a];
+            boolean[] visited = new boolean[10000];
             q.add(String.valueOf(a));
             q.add("");
             String code;
@@ -32,77 +32,113 @@ public class Problem_9019 {
                     break;
                 }
 
-                if(2 * n > 9999) {
-                    q.add(String.valueOf(2 * n % 10000));
-                    q.add(code + "D");
-                }
-
-                else {
-                    q.add(String.valueOf(2 * n));
-                    q.add(code + "D");
-                }
-
-                if(n == 0) {
-                    q.add("9999");
-                    q.add(code + "S");
-                }
-
-                else {
-                    q.add(String.valueOf(n-1));
-                    q.add(code + "S");
-                }
-
-                String temp = "";
-                if(n < 1000 && n >= 100) {
-                    sb.append("0");
-                    sb.append(String.valueOf(n).charAt(0));
-                    sb.append(String.valueOf(n).charAt(1));
-                    sb.append(String.valueOf(n).charAt(2));
-                    temp = sb.toString();
-                }
-
-                else if(n < 100 && n >= 10) {
-                    sb.append("0");
-                    sb.append("0");
-                    sb.append(String.valueOf(n).charAt(0));
-                    sb.append(String.valueOf(n).charAt(1));
-                    temp = sb.toString();
-                }
-
-                else if(n < 10) {
-                    sb.append("0");
-                    sb.append("0");
-                    sb.append("0");
-                    sb.append(String.valueOf(n).charAt(0));
-                    temp = sb.toString();
-                }
-
-                else
-                    temp = String.valueOf(n);
-
-                sb.delete(0, 5);
-
-                sb.append(temp.charAt(1));
-                sb.append(temp.charAt(2));
-                sb.append(temp.charAt(3));
-                sb.append(temp.charAt(0));
-
-                q.add(sb.toString());
-                q.add(code + "L");
-                sb.delete(0, 5);
-
-                sb.append(temp.charAt(3));
-                sb.append(temp.charAt(0));
-                sb.append(temp.charAt(1));
-                sb.append(temp.charAt(2));
-
-                q.add(sb.toString());
-                q.add(code + "R");
-                sb.delete(0, 5);
+                D(n, code, q, visited);
+                S(n, code, q, visited);
+                String temp = createTemp(n, sb);
+                L(n, code, q, visited, sb, temp);
+                R(n, code, q, visited, sb, temp);
             }
             bw.write(code + "\n");
         }
         bw.flush();
         bw.close();
+    }
+
+    public static void D(int n, String code, Queue q, boolean[] visited) {
+        if(2 * n > 9999) {
+            if(!visited[2 * n % 10000]) {
+                q.add(String.valueOf(2 * n % 10000));
+                q.add(code + "D");
+                visited[2 * n % 10000] = true;
+            }
+        }
+
+        else {
+            if(!visited[2 * n]) {
+                q.add(String.valueOf(2 * n));
+                q.add(code + "D");
+                visited[2 * n] = true;
+            }
+        }
+    }
+
+    public static void S(int n, String code, Queue q, boolean[] visited) {
+        if(n == 0) {
+            if(!visited[9999]) {
+                q.add("9999");
+                q.add(code + "S");
+                visited[9999] = true;
+            }
+        }
+
+        else {
+            if(!visited[n-1]) {
+                q.add(String.valueOf(n - 1));
+                q.add(code + "S");
+                visited[n - 1] = true;
+            }
+        }
+    }
+
+    public static String createTemp(int n, StringBuilder sb) {
+        String temp = "";
+        if(n < 1000 && n >= 100) {
+            sb.append("0");
+            sb.append(String.valueOf(n).charAt(0));
+            sb.append(String.valueOf(n).charAt(1));
+            sb.append(String.valueOf(n).charAt(2));
+            temp = sb.toString();
+        }
+
+        else if(n < 100 && n >= 10) {
+            sb.append("0");
+            sb.append("0");
+            sb.append(String.valueOf(n).charAt(0));
+            sb.append(String.valueOf(n).charAt(1));
+            temp = sb.toString();
+        }
+
+        else if(n < 10) {
+            sb.append("0");
+            sb.append("0");
+            sb.append("0");
+            sb.append(String.valueOf(n).charAt(0));
+            temp = sb.toString();
+        }
+
+        else
+            temp = String.valueOf(n);
+
+        sb.delete(0, 5);
+
+        return temp;
+    }
+
+    public static void L(int n, String code, Queue q, boolean[] visited, StringBuilder sb, String temp) {
+        sb.append(temp.charAt(1));
+        sb.append(temp.charAt(2));
+        sb.append(temp.charAt(3));
+        sb.append(temp.charAt(0));
+
+        if(!visited[Integer.parseInt(sb.toString())]) {
+            q.add(sb.toString());
+            q.add(code + "L");
+            visited[Integer.parseInt(sb.toString())] = true;
+        }
+        sb.delete(0, 5);
+    }
+
+    public static void R(int n, String code, Queue q, boolean[] visited, StringBuilder sb, String temp) {
+        sb.append(temp.charAt(3));
+        sb.append(temp.charAt(0));
+        sb.append(temp.charAt(1));
+        sb.append(temp.charAt(2));
+
+        if(!visited[Integer.parseInt(sb.toString())]) {
+            q.add(sb.toString());
+            q.add(code + "R");
+            visited[Integer.parseInt(sb.toString())] = true;
+        }
+        sb.delete(0, 5);
     }
 }
