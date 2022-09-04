8154c375a1ddb9647424456bb353390dd3f627eb
wintee <b12e34e56@naver.com>
2022-09-04 22:30:22 +0900

    G2 11444 피보나치 수 6 해결

--- src/gold/Problem_11444.java
+++ src/gold/Problem_11444.java
@@ -1,35 +1,39 @@
 package gold;
 
 import java.io.*;
-import java.math.BigInteger;
-
 public class Problem_11444 {
+    static long[][] fibo1 = {{1, 1}, {1, 0}};
+    static final int MOD = 1000000007;
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        BigInteger n = new BigInteger(br.readLine());
+        long n = Long.parseLong(br.readLine());
 
+        bw.write(String.valueOf(fibonacci(n)[0][1] % MOD));
+        bw.flush();
+        bw.close();
+    }
+//1000000000000000000
+    public static long[][] fibonacci(long n) {
+        if(n == 1)
+            return fibo1;
+
+        long[][] result;
+        long[][] parameter = fibonacci(n / 2);
+        if(n % 2 == 0)
+            result = multi(parameter, parameter);
+        else
+            result = multi(multi(parameter, parameter), fibo1);
+
+        return result;
     }
 
-    public static BigInteger fibonacci(BigInteger n) {
-        BigInteger result;
-        if(n.equals(1)) {
-
-        }
-
-        if(n.equals(2)) {
-
-        }
-
-        if(n.mod(BigInteger.valueOf(2)).equals(0)) {
-            BigInteger temp = fibonacci(n.divide(BigInteger.valueOf(2)));
-            result = temp.multiply(temp);
-        }
-
-        else {
-            BigInteger temp = fibonacci(n.divide(BigInteger.valueOf(2)));
-            result = temp.multiply(temp).multiply(fibonacci(BigInteger.valueOf(1)));
-        }
+    public static long[][] multi(long[][] a, long[][] b) {
+        long[][] result = new long[2][2];
+        result[0][0] = (a[0][0] % MOD * b[0][0] % MOD + a[0][1] % MOD * b[1][0] % MOD) % MOD;
+        result[0][1] = (a[0][0] % MOD * b[1][0] % MOD + a[0][1] % MOD * b[1][1] % MOD) % MOD;
+        result[1][0] = (a[1][0] % MOD * b[0][0] % MOD + a[1][1] % MOD * b[1][0] % MOD) % MOD;
+        result[1][1] = (a[1][0] % MOD * b[0][1] % MOD + a[1][1] % MOD * b[1][1] % MOD) % MOD;
 
         return result;
     }
