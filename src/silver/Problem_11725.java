40f212d3bf9075a74bfba7a4e11ea5f885ec1a28
wintee <b12e34e56@naver.com>
2022-09-05 18:23:28 +0900

    S2 11725 트리의 부모 찾기 해결

--- src/silver/Problem_11725.java
+++ src/silver/Problem_11725.java
@@ -1,18 +1,18 @@
 package silver;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Problem_11725 {
+    static int[] result;
     static boolean[] visited;
-    static int count = 0;
     public static void main(String[] args) throws IOException {
-        var br = new BufferedReader(new InputStreamReader(System.in));
         var bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        var br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayList<Integer>[] edge = new ArrayList[n - 1];
-        for(int i = 0; i < n - 1; i++)
+        result = new int[n];
+        ArrayList<Integer>[] edge = new ArrayList[n];
+        for(int i = 0; i < n; i++)
             edge[i] = new ArrayList<>();
 
         for(int i = 0; i < n - 1; i++) {
@@ -24,17 +24,32 @@ public class Problem_11725 {
         }
 
         visited = new boolean[n];
+        BFS(1, edge, bw);
+        for(int i = 1; i < n; i++)
+            bw.write(result[i] + "\n");
+
+        bw.flush();
+        bw.close();
     }
 
-    public static void DFS(int n, int target, int prev, ArrayList<Integer>[] edge) {
-        if(n == target) {
-            count = prev;
-            return;
-        }
+    public static void BFS(int start, ArrayList<Integer>[] edge, BufferedWriter bw) throws IOException {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        q.add(start);
+        visited[start - 1] = true;
 
-        for(int i = 0; i < edge[n - 1].size(); i++) {
-            if(!visited[edge[n - 1].get(i)]) {
-                DFS(edge[n - 1].get(i), target, n, edge);
+        while(!q.isEmpty()) {
+            int n = q.remove();
+            int prev = q.remove();
+            if(result[n - 1] == 0)
+                result[n - 1] = prev;
+
+            for(int i = 0; i < edge[n - 1].size(); i++) {
+                if(!visited[edge[n - 1].get(i) - 1]) {
+                    q.add(edge[n - 1].get(i));
+                    q.add(n);
+                    visited[edge[n - 1].get(i) - 1] = true;
+                }
             }
         }
     }
