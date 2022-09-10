0a5015960e75c9c4503c5dfedb9acd9af2796515
winteeeee <b12e34e56@naver.com>
2022-09-10 12:33:12 +0900

    P5 14003 가장 긴 증가하는 부분 수열 5 해결

--- src/platinum/Problem_14003.java
+++ src/platinum/Problem_14003.java
@@ -11,40 +11,55 @@ public class Problem_14003 {
         int n = Integer.parseInt(br.readLine());
         int[] sequence = new int[n + 1];
         long[] dp = new long[n + 1];
+        long[] tracking = new long[n + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++)
             sequence[i] = Integer.parseInt(st.nextToken());
 
 
-        int toIndex = 2;
-        for(int i = 1; i <= n; i++) {
-            if(i == 1)
-                dp[i] = sequence[i];
+        if(n != 1) {
+            int toIndex = 2;
+            int maxIdx = -1;
+            for (int i = 1; i <= n; i++) {
+                if (i == 1) {
+                    dp[i] = sequence[i];
+                    tracking[i] = i;
+                } else {
+                    int idx = Arrays.binarySearch(dp, 1, toIndex, sequence[i]);
+                    if (idx < 0) {
+                        idx = (idx * -1) - 1;
 
-            else {
-                int idx = Arrays.binarySearch(dp, 1, toIndex, sequence[i]);
-                if (idx < 0) {
-                    idx = (idx * -1) - 1;
+                        if (idx == toIndex)
+                            toIndex++;
+                    }
 
-                    if(idx == toIndex)
-                        toIndex++;
+                    dp[idx] = sequence[i];
+                    tracking[i] = idx;
+
+                    if (maxIdx < idx)
+                        maxIdx = idx;
                 }
-
-                dp[idx] = sequence[i];
             }
+
+            long[] result = new long[maxIdx + 1];
+            for (int i = n; i >= 1; i--) {
+                if (tracking[i] == maxIdx) {
+                    result[(int) tracking[i]] = sequence[i];
+                    maxIdx--;
+                }
+            }
+
+            int size = result.length;
+            bw.write(size - 1 + "\n");
+            for (int i = 1; i < size; i++)
+                bw.write(result[i] + " ");
         }
 
-        int size = 0;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; true; i++, size++) {
-            if(dp[i] == 0)
-                break;
-            sb.append(dp[i] + " ");
+        else {
+            bw.write(1 + "\n");
+            bw.write(String.valueOf(sequence[1]));
         }
-
-        bw.write(size + "\n");
-        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
