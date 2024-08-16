81ac172ca624114b743283bfd296bf9193d196e6
wintee-laptop <b12e34e56@naver.com>
2024-08-16 16:54:51 +0100

    G4 11054 가장 긴 바이토닉 부분 수열 재해결

--- src/gold/Problem_11054.java
+++ src/gold/Problem_11054.java
@@ -1,8 +1,17 @@
 package gold;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+
+/*
+재채점으로 틀려서 기존 코드를 고쳐서 다시 풀었다.
+그냥 DP 테이블은 이해가 갔는데 DP2를 왜 저렇게 채우는지 이해가 되지 않아 DP2 부분을 다시 풀었다.
+
+DP1을 해당 원소까지 보았을 때 가장 큰 증가하는 부분 수열의 길이를 저장해두고
+DP2은 끝에서 첫 번째 원소로 가며 가장 큰 증가하는 부분 수열의 길이를 저장한다.
+
+DP1 + DP2의 합이 최대인 것이 답이다.
+ */
 
 public class Problem_11054 {
     public static void main(String[] args) throws IOException {
@@ -11,6 +20,7 @@ public class Problem_11054 {
         int n = Integer.parseInt(br.readLine());
         int[] sequence = new int[n + 1];
         int[] dp = new int[n + 1];
+        int[] dp2 = new int[n + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++)
@@ -24,26 +34,18 @@ public class Problem_11054 {
             }
         }
 
-        int result = 0;
-        for(int i = 1; i <= n; i++) {
-            int[] dp2 = new int[n + 1];
-            int max = 0;
-            for(int j = i + 1; j <= n; j++) {
-                for(int k = 0; k < j; k++) {
-                    if (sequence[j] < sequence[k]) {
-                        dp2[j] = Math.max(dp2[k] + 1, dp2[j]);
-
-                        if (dp2[j] > max)
-                            max = dp2[j];
-                    }
+        for(int i = n; i >= 0; i--) {
+            for (int j = n; j > i; j--) {
+                if(sequence[i] > sequence[j]) {
+                    dp2[i] = Math.max(dp2[j] + 1, dp2[i]);
                 }
-            }
-
-            if (dp[i] + max > result) {
-                result = dp[i] + max;
             }
         }
 
+
+        int result = 0;
+        for (int i = 1; i <= n; i++)
+            result = Math.max(dp[i] + dp2[i], result);
         bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
